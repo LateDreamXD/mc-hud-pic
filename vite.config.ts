0acd20ae5d64154b54b8f6d56bfe7dacc1551524
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-03-21 17:44:17 +0800

    perf(vite-conf): optimize code-spliting

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import type { UserConfig } from 'vite';
 import vue from 'unplugin-vue/vite';
 
 export default <UserConfig>{
-	plugins: [vue()],
+	build: {
+		rolldownOptions: {
+			output: {
+				codeSplitting: {
+					groups: [
+						{ name: 'vue-bundle', test: /node_modules\/vue$/ },
+						{ name: 'i18n', test: /src\/i18n/ },
+						{ name: 'utils', test: /src\/utils/ }
+					]
+				}
+			}
+		}
+	},
+	plugins: [vue()]
 };
