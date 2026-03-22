2142f97bc22de938f33cba666bb26e9fbfd729b0
LateDreamXD <127290936+LateDreamXD@users.noreply.github.com>
2026-03-22 19:29:26 +0800

    chore(style): split scss from App.vue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import i18n, { loadCurrentLocale } from './i18n';
+
+import './index.scss';
+
 import VueLateWindowManager from 'vue-late-window-manager';
 import 'vue-late-window-manager/style';
 
